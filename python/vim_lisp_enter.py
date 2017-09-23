27b3413c00786eeda4e0b949a7259542b635e1d8
Leander Jedamus <ljedamus@web.de>
2017-09-23 16:40:29 +0200

    minor changes

--- python/vim_lisp_enter.py
+++ python/vim_lisp_enter.py
@@ -2,12 +2,12 @@
 # coding=utf-8
 
 # erzeugt Samstag, 23. September 2017 12:57 (C) 2017 von Leander Jedamus
-# modifiziert Samstag, 23. September 2017 16:36 von Leander Jedamus
+# modifiziert Samstag, 23. September 2017 16:40 von Leander Jedamus
 
 import pyvim as p
 
 p.drs()
-p.sr("a","iich bin in lisp.\n")
+#p.sr("a","iich bin in lisp.\n")
 
 # vim:ai sw=2 sts=4 expandtab
 
