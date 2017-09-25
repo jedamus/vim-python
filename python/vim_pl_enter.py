18b78b7f33d2f076b001e4d388749bc36b7fa958
Leander Jedamus <ljedamus@web.de>
2017-09-25 20:31:33 +0200

    changed registers to use ai

--- python/vim_pl_enter.py
+++ python/vim_pl_enter.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 # erzeugt Samstag, 23. September 2017 12:57 (C) 2017 von Leander Jedamus
+# modifiziert Montag, 25. September 2017 20:30 von Leander Jedamus
 # modifiziert Samstag, 23. September 2017 23:46 von Leander Jedamus
 
 import pyvim as p
@@ -11,23 +12,23 @@ p.drs()
 p.sr("g","iuse Locale::gettext qw( gettext bindtextdomain textdomain bind_textdomain_codeset );\nuse POSIX;\nuse FindBin '$Bin';\nuse File::Spec;\n\nmy $domain = \"mycopy.pl\";\nbindtextdomain($domain,File::Spec->catfile($Bin,\"translate\"));\ntextdomain($domain);\nbind_textdomain_codeset($domain,\"UTF-8\");\nsub _ ($) { &gettext; }\n5k14l")
 
 # if:
-p.sr("i",":se noai\niif () {\n  \n};# if:se ai\n^2kwl")
+p.sr("i",":se ai\nAif () {\n};# ifkA\n  ^kwl")
 # unless:
-p.sr("u",":se noai\niunless () {\n  \n};# unless:se ai\n^2kwl")
+p.sr("u",":se ai\nAunless () {\n};# unlesskA\n  ^kwl")
 # else:
-p.sr("e",":se noai\n/;# \ns A\nelse {\n  \n};# else:se ai\n^k2l")
+p.sr("e",":se ai\n/;# \ns A\nelse {\n};# elsekA\n  ")
 # elsif:
-p.sr("l",":se noai\n/;# \ns A\nelsif () {\n  \n};# elsif:se ai\n^2kwl")
+p.sr("l",":se ai\n/;# \ns A\nelsif () {\n};# elsifkA\n  ^kwl")
 # while
-p.sr("w",":se noai\niwhile () {\n  \n};# while:se ai\n^2kwl")
+p.sr("w",":se ai\nAwhile () {\n};# whilekA\n  ^kwl")
 # uNtil:
-p.sr("n",":se noai\niuntil () {\n  \n};# until:se ai\n^2kwl")
+p.sr("n",":se ai\nAuntil () {\n};# untilkA\n  ^kwl")
 # do while
-p.sr("d",":se noai\nido {\n  \n} while :se ai\n")
+p.sr("d",":se ai\nAdo {\n} while kA\n  j6l")
 # for
-p.sr("f",":se noai\nifor (;;) {\n  \n};# for:se ai\n^2kwl")
+p.sr("f",":se ai\nAfor (my $;;) {\n};# forkA\n  ^k3wl")
 # fOreach
-p.sr("o",":se noai\niforeach my $ (@list) {\n  \n};# foreach:se ai\n^2kw4l")
+p.sr("o",":se ai\nAforeach my $ (@list) {\n};# foreachkA\n  ^kw4l")
 
 # vim:ai sw=2 sts=4 expandtab
 
