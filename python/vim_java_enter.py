03720f89c0b15f233b2aad75754fd863f2c6c93e
Leander Jedamus <ljedamus@web.de>
2017-10-07 14:33:25 +0200

    added java registers

---
+++ python/vim_java_enter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# erzeugt Samstag, 07. Oktober 2017 13:57 (C) 2017 von Leander Jedamus
+# modifiziert Samstag, 07. Oktober 2017 14:32 von Leander Jedamus
+
+import pyvim as p
+
+p.drs()
+p.sr("a","iich bin in java.\n")
+# if:
+p.sr("i",":se ai\nAif () {\n};// ifkA\n  ^kwl")
+# else:
+p.sr("e",":se ai\n/;// \ns A\nelse {\n};// elsekA\n  ")
+# switch
+p.sr("s",":se ai\nAswitch () {\n};// switchkA\n  case :\ncase :\ndefault:^2kA\n  break;jA\n  break;jA\n  break;7k2wl")
+# while
+p.sr("w",":se ai\nAwhile () {\n};// whilekA\n  ^kwl")
+# do while
+p.sr("d",":se ai\nAdo {\n} while ();kA\n  j8l")
+# for
+p.sr("f",":se ai\nAfor (;;) {\n};// forkA\n  ^kwl")
+
+# vim:ai sw=2 sts=4 expandtab
+
