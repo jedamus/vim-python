4d64b3c16cf22f649dd924c4c7dc0a9e5b6d9ac6
Leander Jedamus <ljedamus@web.de>
2018-07-13 13:17:15 +0200

    minor changes

--- python/vim_c_enter.py
+++ python/vim_c_enter.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 # erzeugt Samstag, 23. September 2017 12:57 (C) 2017 von Leander Jedamus
-# modifiziert Freitag, 13. Juli 2018 12:18 von Leander Jedamus
+# modifiziert Freitag, 13. Juli 2018 13:14 von Leander Jedamus
 # modifiziert Donnerstag, 12. Juli 2018 22:41 von Leander Jedamus
 # modifiziert Donnerstag, 19. Oktober 2017 14:56 von Leander Jedamus
 # modifiziert Samstag, 23. September 2017 16:37 von Leander Jedamus
@@ -16,7 +16,7 @@ p.sr("e",":se ai\n:se nocindent\n/};\n2s} \eAelse {\n};\ekA\n  \e:se cindent\nk$
 p.sr("l",":se ai\n:se nocindent\n/};\n2s} \eAelse if () {\n};\ekA\n  \e:se cindent\nk3wl")
 p.sr("f",":se ai\n:se nocindent\nA\nfor (;;) {\n};\ekA\n  \e:se cindent\nkwl")
 p.sr("s",":se ai\n:se nocindent\nA\nswitch () {\n};\ekA\n  case :\ncase :\ndefault:\ekkA\n         break;\ejA\n         break;\ejA\n         break;\e:se cindent\n6k^wl")
-p.sr("d",":se ai\n:se nocindent\nA\ndo {\nwhile ();\ekA\n  \e:se cindent\nj$h")
+p.sr("d",":se ai\n:se nocindent\nA\ndo {\n} while ();\ekA\n  \e:se cindent\nj$h")
 p.sr("w",":se ai\n:se nocindent\nA\nwhile () {\n};\ekA\n  \e:se cindent\nkwl")
 
 # vim:ai sw=2 sts=4 expandtab
