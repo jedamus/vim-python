b0f5e05147910f45e3092d9b07d6bdd0c35f327f
Leander Jedamus <ljedamus@web.de>
2018-07-13 14:10:26 +0200

    added register for main function

--- python/vim_c_enter.py
+++ python/vim_c_enter.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 # erzeugt Samstag, 23. September 2017 12:57 (C) 2017 von Leander Jedamus
-# modifiziert Freitag, 13. Juli 2018 13:14 von Leander Jedamus
+# modifiziert Freitag, 13. Juli 2018 14:08 von Leander Jedamus
 # modifiziert Donnerstag, 12. Juli 2018 22:41 von Leander Jedamus
 # modifiziert Donnerstag, 19. Oktober 2017 14:56 von Leander Jedamus
 # modifiziert Samstag, 23. September 2017 16:37 von Leander Jedamus
@@ -18,6 +18,7 @@ p.sr("f",":se ai\n:se nocindent\nA\nfor (;;) {\n};\ekA\n  \e:se cindent\nkwl")
 p.sr("s",":se ai\n:se nocindent\nA\nswitch () {\n};\ekA\n  case :\ncase :\ndefault:\ekkA\n         break;\ejA\n         break;\ejA\n         break;\e:se cindent\n6k^wl")
 p.sr("d",":se ai\n:se nocindent\nA\ndo {\n} while ();\ekA\n  \e:se cindent\nj$h")
 p.sr("w",":se ai\n:se nocindent\nA\nwhile () {\n};\ekA\n  \e:se cindent\nkwl")
+p.sr("m",":se ai\n:se nocindent\nA\nint main(int argc, char * argv[]) {\n};\ekA\n  \e:se cindent\n")
 
 # vim:ai sw=2 sts=4 expandtab
 
