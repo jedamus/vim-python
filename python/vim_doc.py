b3d9a20e30a64775aac3ce0a2b25c6af28b851a7
Leander Jedamus <ljedamus@web.de>
2019-05-22 17:58:46 +0200

    added Makefile to create html from *.py-files

---
+++ python/vim_doc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8 -*- python -*-
+
+# erzeugt Dienstag, 21. Mai 2019 13:02 (C) 2019 von Leander Jedamus
+# modifiziert Mittwoch, 22. Mai 2019 14:56 von Leander Jedamus
+# modifiziert Dienstag, 21. Mai 2019 15:16 von Leander Jedamus
+
+from __future__ import print_function
+
+def command(str):
+  pass
+
+class Current():  
+  """
+  """
+class Buffer():
+  """
+  """
+  def __setitem__(self,str1,str2):
+    pass
+
+  def __getitem__(self,str):
+    return("test")
+
+current = Current()
+current.buffer = Buffer()
+current.buffer.name = "test"
+
+# vim:ai sw=2 sts=4 expandtab
+
