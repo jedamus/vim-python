1b1571bf07be6492cd8d7f424baa135097a62861
Leander Jedamus <ljedamus@web.de>
2020-11-21 13:56:29 +0100

    added shortened PATH to make_html.sh

--- python/make_html.sh
+++ python/make_html.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 # erzeugt Mittwoch, 22. Mai 2019 17:53 (C) 2019 von Leander Jedamus
+# modifiziert Samstag, 21. November 2020 13:55 von Leander Jedamus
 # modifiziert Sonntag, 26. Mai 2019 11:58 von Leander Jedamus
 # modifiziert Mittwoch, 22. Mai 2019 18:47 von Leander Jedamus
+
+export PATH=/usr/local/bin:/usr/bin:/bin
 
 #mv vim_doc.py vim.py
 make
