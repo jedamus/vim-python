b4801702c7ed6aeee7d86143e20c7e4817e535fd
Leander Jedamus <ljedamus@web.de>
2020-12-14 23:43:23 +0100

    minor changes

--- install3.sh
+++ install3.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:29 (C) 2020 von Leander Jedamus
-# modifiziert Montag, 14. Dezember 2020 14:43 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 23:42 von Leander Jedamus
 
 vim=$HOME/.vim
 
 cd $HOME
-ln -svf $vim/vimrc3 .vimrc
+ln -svf .vim/vimrc3 .vimrc
 cp -vp $vim/.exrc $HOME
 
 # vim:ai sw=2
