e90a1b1423508cbff1f52fc09b43948b4100b052
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:32:25 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:29 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:31 von Leander Jedamus
+
+cd $HOME
+ln -sf .vim/vimrc3 .vimrc
+cp -p .vim/.exrc $HOME
+
+# vim:ai sw=2
+
