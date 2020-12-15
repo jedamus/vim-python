5106f89ac1399d5c61e158bc26ad5591e92b4453
Leander Jedamus <ljedamus@web.de>
2020-12-15 10:46:20 +0100

    moved from ln to cp

--- install2.sh
+++ install2.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:29 (C) 2020 von Leander Jedamus
+# modifiziert Dienstag, 15. Dezember 2020 10:45 von Leander Jedamus
 # modifiziert Montag, 14. Dezember 2020 23:42 von Leander Jedamus
 
 vim=$HOME/.vim
 
 cd $HOME
-ln -svf .vim/vimrc2 .vimrc
+cp -vp $vim/vimrc2 .vimrc
 cp -vp $vim/.exrc $HOME
 
 # vim:ai sw=2
