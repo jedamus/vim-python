041d8042443b7c6911e29c38bfc97cd5cc8ee1af
Leander Jedamus <ljedamus@web.de>
2020-12-15 10:49:05 +0100

    minor changes

--- install2.sh
+++ install2.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:29 (C) 2020 von Leander Jedamus
-# modifiziert Dienstag, 15. Dezember 2020 10:45 von Leander Jedamus
+# modifiziert Dienstag, 15. Dezember 2020 10:48 von Leander Jedamus
 # modifiziert Montag, 14. Dezember 2020 23:42 von Leander Jedamus
 
 vim=$HOME/.vim
 
-cd $HOME
-cp -vp $vim/vimrc2 .vimrc
+cp -vp $vim/vimrc2 $HOME/.vimrc
 cp -vp $vim/.exrc $HOME
 
 # vim:ai sw=2
